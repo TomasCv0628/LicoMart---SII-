96e35222d4384b9e8574283f637a266d5b6a2ed3
TomasCv0628 <tomas2224508@correo.uis.edu.co>
2025-09-22 17:44:12 -0500

    feat(auth): registro, inicio y cierre de sesión con autenticación en BD feat(ui): navbar condicional para usuarios autenticados

--- hub/urls.py
+++ hub/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('hello/<str:username>', views.hello), 
+    path('', views.index ,name='home'),
+    path('singup/', views.singup, name="singup"), 
+    path('pedido/', views.pedidos, name="pedido"), 
+    path('logout/', views.singout, name="logout"), 
+    path('singin/', views.singin, name="singin"), 
+
+
     
 ]
--- hub/views.py
+++ hub/views.py
@@ -1,9 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login , logout, authenticate
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html') 
 
-def hello(request , username):
-    return HttpResponse("<h1>Hello %s</h1> "% username)
+def singup(request):
+    if request.method == 'GET':
+        return render(request, 'singup.html', {
+        'form': UserCreationForm
+    }) 
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'] , password=request.POST['password1'])
+                user.save()
+                login(request , user)
+                return redirect('pedido')
+            except:
+                return render(request, 'singup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Ya existente'
+                })
+            
+        return render(request, 'singup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Constraseñas no coinciden'
+        })
+
+def pedidos(request):
+    return render(request , 'pedido.html')
+
+def singout(request):
+    logout(request)
+    return redirect('home')
+
+def singin(request):
+    if request.method == 'GET':
+        return render(request, 'singin.html', {
+        'form': AuthenticationForm
+    }) 
+    else:
+        user = authenticate(request, username= request.POST['username'] , password=request.POST['password'])
+        if user is None:
+            return render(request, 'singin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o Constraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
+
