f11faef0cabf3d3d9fe39a7201b9af2f19351e34
JuanGal99 <juanpablogalvisperez10@gmail.com>
2025-10-02 11:35:54 -0500

    fix: corregir rutas de acceso en los archivos de urls

--- backend/productos/urls.py
+++ backend/productos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.lista_productos, name='lista_productos'),
+    path('', views.lista_productos, name='lista_productos'),
     path('crear/', views.crear_producto, name='crear_producto')
 ]
