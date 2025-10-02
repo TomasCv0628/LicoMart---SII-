75ae65c26d1380c4ca75b557f81826b3ead4d4f4
TomasCv0628 <tomas2224508@correo.uis.edu.co>
2025-10-02 16:12:54 -0500

    Cambio forma de almacenar las imagenes IMGfield to URLfield

--- backend/productos/models.py
+++ backend/productos/models.py
@@ -7,7 +7,7 @@ class Productos(models.Model):
     descripcion = models.CharField(max_length=200, blank=True, null=True)
     stock = models.IntegerField()
     precio = models.FloatField()
-    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
+    imagen = models.URLField(max_length=500, blank=True, null=True)  # Cambiado a URLField
 
     def __str__(self):
         return self.nombre
